c92afe3a8be5e6ef98e1e1e6c4a6615929d24df3
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-06 23:42:29 +0100

    fix(coords): Change methods visibility

--- src/main/java/com/night/osmrouteplanner/graphics/Coordinates.java
+++ src/main/java/com/night/osmrouteplanner/graphics/Coordinates.java
@@ -5,24 +5,24 @@ public class Coordinates {
     private double lon;
     private double lat;
 
-    Coordinates(double lon, double lat) {
+    public Coordinates(double lon, double lat) {
         this.lon = lon;
         this.lat = lat;
     }
 
-    protected double getLon() {
+    public double getLon() {
         return lon;
     }
 
-    protected double getLat() {
+    public double getLat() {
         return lat;
     }
 
-    protected void setLon(double lon) {
+    public void setLon(double lon) {
         this.lon = lon;
     }
 
-    protected void setLat(double lat) {
+    public void setLat(double lat) {
         this.lat = lat;
     }
 }
