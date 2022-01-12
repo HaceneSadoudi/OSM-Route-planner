4c26292a3f7dd4c5a680b31d4c16c68e0c7e9777
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-12 23:40:20 +0100

    feat: Add converter for longitude and latitude

---
+++ src/main/java/com/night/osmrouteplanner/Converter.java
@@ -0,0 +1,13 @@
+package com.night.osmrouteplanner;
+
+public class Converter {
+
+    private double[] arrayOfLon;
+    private double[] arrayOfLat;
+
+
+    public Converter(double[] parserArrayLon, double[] parserArrayLat) {
+        arrayOfLon = parserArrayLon;
+        arrayOfLat = parserArrayLat;
+    }
+}
