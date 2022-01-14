8711673ad892a38d06f499e5334907a30f572a61
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-14 01:36:18 +0100

    chore(parser): Add getters and setters

--- src/main/java/com/night/osmrouteplanner/Parser.java
+++ src/main/java/com/night/osmrouteplanner/Parser.java
@@ -1,17 +1,22 @@
 package com.night.osmrouteplanner;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Parser {
+
+    private long[] wayId;
 
     private NodeList listWay;
     private NodeList listRelation;
@@ -21,6 +26,11 @@ public class Parser {
     private Map<Long, List<Double>> waysLongitude;
 
 
+    Parser() {
+        this.waysLatitude = new HashMap<>();
+        this.waysLongitude = new HashMap<>();
+
+    }
     void init(File data) {
         listWay = getNodesByType(data, "way");
         listRelation = getNodesByType(data,"relation");
@@ -41,4 +51,54 @@ public class Parser {
         }
         return null;
     }
+
+    public long[] getWayId() {
+        return wayId;
+    }
+
+    public void setWayId(long[] wayId) {
+        this.wayId = wayId;
+    }
+
+    public NodeList getListWay() {
+        return listWay;
+    }
+
+    public void setListWay(NodeList listWay) {
+        this.listWay = listWay;
+    }
+
+    public NodeList getListRelation() {
+        return listRelation;
+    }
+
+    public void setListRelation(NodeList listRelation) {
+        this.listRelation = listRelation;
+    }
+
+    public NodeList getListMember() {
+        return listMember;
+    }
+
+    public void setListMember(NodeList listMember) {
+        this.listMember = listMember;
+    }
+
+    public Map<Long, List<Double>> getWaysLatitude() {
+        return waysLatitude;
+    }
+
+    public void setWaysLatitude(Map<Long, List<Double>> waysLatitude) {
+        this.waysLatitude = waysLatitude;
+    }
+
+    public Map<Long, List<Double>> getWaysLongitude() {
+        return waysLongitude;
+    }
+
+    public void setWaysLongitude(Map<Long, List<Double>> waysLongitude) {
+        this.waysLongitude = waysLongitude;
+    }
+
+
 }
