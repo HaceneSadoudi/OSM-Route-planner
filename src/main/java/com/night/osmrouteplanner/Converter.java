d500a06176279ffa8cbdb824245ffeba7c59b4f8
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2022-01-15 23:49:21 +0100

    feat: Convert longitude to pixel

--- src/main/java/com/night/osmrouteplanner/Converter.java
+++ src/main/java/com/night/osmrouteplanner/Converter.java
@@ -10,4 +10,16 @@ public class Converter {
         arrayOfLon = parserArrayLon;
         arrayOfLat = parserArrayLat;
     }
+
+
+    public double[] convertLon(double minLon) {
+        int nbrOfPoints = arrayOfLon.length;
+        double[] lonX = new double[nbrOfPoints];
+
+        for(int i=0;i<nbrOfPoints;i++) {
+            double minLonPixel = 500 * ((minLon + 180) / 360);
+            double xPixel = 500 * ((arrayOfLat[i] + 180) / 360);
+        }
+        return lonX;
+    }
 }
